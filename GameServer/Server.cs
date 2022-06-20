fc56d6a74d72100deb03cf16004c03e218d37ffc
KABOOTAR-1 <bt20cse052@iiitn.ac.in>
2022-06-20 20:43:16 +0530

    Server Change

---
+++ GameServer/Server.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Server
+    {
+        public static int maxPlayers { get; private set; }
+        public static int Port { get; private set; }
+        private static TcpListener Tcplistener;
+        public static void Start(int _maxplayers,int _port)
+        {
+            maxPlayers = _maxplayers;
+            Port = _port;
+            Tcplistener = new TcpListener(IPAddress.Any,Port);
+        }
+
+
+    }
+}
